b0bdc13a1134d5943b3224e943c03513252aea71
phasedstar <60366467+phasedstar@users.noreply.github.com>
2020-05-11 12:29:10 -0500

    final

--- docs/library/jquery.js
+++ docs/library/jquery.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+      
+  $(".button").click(function(){
+    $(".modal").toggle();
+  });
+  
+  $(".modal-background").click(function(){
+    $(".modal").toggle();
+  })
+  
+  $(".close").click(function(){
+    $(".modal").toggle();
+  })
+  
+
+  
+  
 
           
 })
