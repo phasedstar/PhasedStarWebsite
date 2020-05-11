ff92d7d8c2f4edc2c3786e0f27eb1a637767a366
phasedstar <60366467+phasedstar@users.noreply.github.com>
2020-05-11 17:02:29 -0500

    rid of docs

--- docs/library/jquery.js
+++ docs/library/jquery.js
@@ -4,10 +4,6 @@ $(document).ready(function(){
     $(".modal").toggle();
   });
   
-  $(".modal-background").click(function(){
-    $(".modal").toggle();
-  })
-  
   $(".close").click(function(){
     $(".modal").toggle();
   })
